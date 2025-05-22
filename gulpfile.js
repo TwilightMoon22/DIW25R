df973d05de1db0bb880daaf3e52eeec113df217c
TwilightMoon22 <trabajopbg@gmail.com>
2025-05-22 02:59:26 +0200

    proyecto arreglado y terminado, por fin

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,58 @@
 const { src, dest, watch, series, parallel } = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
-
-function css(done) {
-    //compilar sass
-    //pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
-    src('src/scss/app.scss')
-        .pipe(sass())
-        //.pipe(sass({outputStyle: 'compressed'})) // con esto le decimos que queremos que el css nos lo haga comprimido, que ocupe lo mínimo
-        .pipe(postcss([autoprefixer()])) // para dar soporte a navegadores antiguos que he tenido que especificar en el package.json
-        .pipe(dest('build/css')) // este es el archivo que debe compilar
-    done();
-}
-
-function imagenes() {
-    return src("src/img/**/*")
-        .pipe(imagemin({ optimizationLevel: 3 }))
-        .pipe(dest("build/img"));
-}
+const sass = require ('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss')
+const autoprefixer  = require('autoprefixer')
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function versionWebp() {
-    const opciones = {
-        quality: 50
-    }
-    return src("src/img/**/*.{png,jpg}")
-        .pipe(webp(opciones))
-        .pipe(dest("build/img"));
+  const opciones = {
+    quality: 50 //quality es la calidad de la imagen, 50 es un valor intermedio.
+  }
+  return src("src/img/**/*.{png,jpg}") //sólo quiero convertir a webp las imágenes png y jpg
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
 }
 
 function versionAvif() {
-    return src("src/img/**/*.{png,jpg}")
-        .pipe(avif())
-        .pipe(dest("build/img"));
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(avif())
+    .pipe(dest("build/img"));
 }
+ 
 
-function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
+//Si no quiero usar el done cómo parámetro, puedo hacerlo así usando return, pero no puedo usar done y return a la vez.
+function imagenes() {
+  return src("src/img/**/*")
+   // IMPORTANTE. LA TAREA DE OPTIMIZACIÓN ANTES DE LA TAREA DE COPIAR LA IMAGEN EN BUILD.
+    .pipe(imagemin({optimizationLevel: 3})) 
+    //imagemin es un plugin que optimiza las imágenes, optimizationLevel: 3 es el nivel de optimización, 3 es el máximo.
+    .pipe(dest("build/img"));
 }
-
+ 
+function css(done) {
+ //compilar sass
+ //pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
+ src('src/scss/app.scss')
+   .pipe(sass())
+    //.pipe(sass({outputStyle: 'compressed'})) // con esto le decimos que queremos que el css nos lo haga comprimido, que ocupe lo mínimo
+    .pipe(postcss([autoprefixer()])) // para dar soporte a navegadores antiguos que he tenido que especificar en el package.json
+   .pipe(dest('build/css')) // este es el archivo que debe compilar
+   done();
+}
+function dev(){
+  watch('src/scss/**/*.scss',css) //modificamos el de arriba para que escuche cambios en cualquier archivo .scss incluido en cualquier
+  //carpeta dentro de la carpeta 'src/scss'
+  watch("src/img/**/*", imagenes);
+}
 exports.css = css;
+exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = dev;
-
-// Tarea por defecto que ejecuta primero css y luego dev
-exports.default = series(imagenes, versionWebp, versionAvif, css, dev); // Mejor series, para que compile primero, y luego se quede escuchando. La tarea con los
+exports.default = series(versionWebp, versionAvif,css, dev); // Mejor series, para que compile primero, y luego se quede escuchando. La tarea con los
 //watch siempre la última
 
 //series -  lanza las tareas una detrás de otra, hasta que no termina una no empieza la otra
-//parallel -  las lanza a la vez, no espera. 
+//parallel -  las lanza a la vez, no espera.
---
+++ progreso7.md
@@ -0,0 +1,38 @@
+# Progreso Sección 7
+
+## Creación de la Estructura Base
+- Creado el archivo `base.html` con la estructura común para todas las páginas
+- Implementado el header y footer comunes
+- Configurada la navegación principal
+
+## Implementación de la Página Nosotros
+- Creado el archivo `nosotros.html` basado en `base.html`
+- Actualizada la clase del header a `header-nosotros`
+- Cambiado el enlace activo en la navegación
+- Modificado el título principal a "Una nueva experiencia cada día"
+- Añadido el contenido principal con imagen y texto
+
+## Estilos CSS
+- Actualizado `_header.scss` para incluir los estilos del header de la página Nosotros
+- Añadida la imagen de fondo específica para la página Nosotros
+- Configurado el `background-repeat` y `background-size` para evitar repetición
+- Creado el archivo `_nosotros.scss` en la carpeta internas
+- Implementado el diseño responsive con grid de dos columnas
+- Añadidos estilos para la imagen rotada y con borde
+- Configurado el estilo del blockquote
+
+## Mejoras Globales
+- Actualizado `_globales.scss` para mejorar el manejo de imágenes
+- Añadida la propiedad `height: auto` para mantener la proporción
+- Mejorada la tipografía y espaciado general
+
+## Estado Actual
+- Página Nosotros implementada correctamente
+- Estructura base creada para futuras páginas
+- Estilos responsive funcionando
+- Imágenes y contenido correctamente posicionados
+
+## Próximos Pasos
+- Implementar las páginas restantes (Proceso, Menú, Galería, Contacto)
+- Revisar y ajustar el diseño responsive
+- Optimizar las imágenes para mejor rendimiento 
